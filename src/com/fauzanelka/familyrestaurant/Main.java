c69d4f6b05939cd12f2163ed3d316dbd286ceafd
Fauzan Elka <fauzan.elka@hotmail.com>
2021-09-25 21:36:55 +0700

    Implement deleteRgularMenu

--- src/com/fauzanelka/familyrestaurant/Main.java
+++ src/com/fauzanelka/familyrestaurant/Main.java
@@ -22,7 +22,7 @@ public class Main {
                 in.next();
                 System.out.println();
             }
-        } while (!"^R[0-9]\\d{0,2}$".matches(menuCode));
+        } while (!menuCode.matches("^R[0-9]\\d{0,2}$"));
 
         String menuName = null;
         do {
@@ -33,7 +33,7 @@ public class Main {
                 in.next();
                 System.out.println();
             }
-        } while (!"^[a-zA-Z0-9]{5,20}$".matches(menuName));
+        } while (!menuName.matches("^[a-zA-Z0-9]{5,20}$"));
 
         int menuPrice = 0;
         do {
@@ -55,11 +55,52 @@ public class Main {
     }
 
     public static void showAllMenu() {
-        // TODO
+        System.out.println("Regular Menu");
+        System.out.println("===");
+        System.out.println("No. | Kode | Nama | Harga");
+        System.out.println("===");
+        for (int i = 0; i < regularItems.size(); i++) {
+            System.out.printf(
+                    "%d | %s | %s | %d %n",
+                    i + 1,
+                    regularItems.get(i).getCode(),
+                    regularItems.get(i).getName(),
+                    regularItems.get(i).getPrice()
+            );
+        }
     }
 
     public static void deleteRegularMenu() {
-        // TODO
+        System.out.println("Delete Regular Menu");
+        System.out.println("===");
+
+        Scanner in = new Scanner(System.in);
+        String menuCode = null;
+
+        boolean isDeleted = false;
+        do {
+            do {
+                System.out.print("Input Menu Code [R...]: ");
+                try {
+                    menuCode = in.next();
+                } catch (InputMismatchException e) {
+                    in.next();
+                    System.out.println();
+                }
+            } while (!menuCode.matches("^R[0-9]\\d{0,2}$"));
+
+            for (int i = 0; i < regularItems.size(); i++) {
+                if (regularItems.get(i).getCode().equals(menuCode)) {
+                    regularItems.remove(i);
+                    isDeleted = true;
+                    System.out.println("Delete Success!");
+                }
+            }
+
+            if (!isDeleted) {
+                System.out.println("Code is Wrong");
+            }
+        } while (!isDeleted);
     }
 
     public static void deleteSpecialMenu() {
@@ -67,48 +108,53 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Family Restaurant");
-        System.out.println("====");
-        System.out.println("1. Add Regular Menu");
-        System.out.println("2. Add Special Menu");
-        System.out.println("3. Show All Menu");
-        System.out.println("4. Delete Regular Menu");
-        System.out.println("5. Delete Special Menu");
-        System.out.println("6. Exit");
-
         int choice = 0;
         List<Integer> choices = new ArrayList<>(
                 Arrays.asList(1, 2, 3, 4, 5, 6)
         );
         Scanner in = new Scanner(System.in);
-        do {
-            try {
-                System.out.print("Choice [1-6]: ");
-                choice = in.nextInt();
-            } catch (InputMismatchException e) {
-                in.next();
-            }
-        } while (!choices.contains(choice));
 
-        switch (choice) {
-            case 1: // Add Regular Menu
-                addRegularMenu();
-                break;
-            case 2: // Add Special Menu
-                addSpecialMenu();
-                break;
-            case 3: // Show All Menu
-                showAllMenu();
-                break;
-            case 4: // Delete Regular Menu
-                deleteRegularMenu();
-                break;
-            case 5: // Delete Special Menu
-                deleteSpecialMenu();
-                break;
-            case 6: // Exit
-                System.exit(0);
-                break;
-        }
+        do {
+            System.out.println("Family Restaurant");
+            System.out.println("====");
+            System.out.println("1. Add Regular Menu");
+            System.out.println("2. Add Special Menu");
+            System.out.println("3. Show All Menu");
+            System.out.println("4. Delete Regular Menu");
+            System.out.println("5. Delete Special Menu");
+            System.out.println("6. Exit");
+
+            do {
+                try {
+                    System.out.print("Choice [1-6]: ");
+                    choice = in.nextInt();
+                } catch (InputMismatchException e) {
+                    in.next();
+                }
+            } while (!choices.contains(choice));
+
+            switch (choice) {
+                case 1: // Add Regular Menu
+                    addRegularMenu();
+                    break;
+                case 2: // Add Special Menu
+                    addSpecialMenu();
+                    break;
+                case 3: // Show All Menu
+                    showAllMenu();
+                    break;
+                case 4: // Delete Regular Menu
+                    deleteRegularMenu();
+                    break;
+                case 5: // Delete Special Menu
+                    deleteSpecialMenu();
+                    break;
+                case 6: // Exit
+                    break;
+            }
+
+        } while (choice != 6);
+
+        in.close();
     }
 }
